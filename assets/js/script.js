ba4c3ad01a7b9ac978e483122f3baf98598d16b2
brennaveir <brennaveir@hotmail.com>
2022-12-20 12:07:57 -0500

    linked CSS/JS files to HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+console.log("linked!")
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
